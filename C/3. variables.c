705177a700b8d5e71919e4487de67dc6e5669124
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-04-12 15:11:14 +0600

    Add files via upload

---
+++ C/3. variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int total_students;
+    total_students = 100;
+    total_students = total_students + 10;
+    printf("Total number of students %d\n", total_students);
+    int absent_student;
+    absent_student = 5;
+    printf("Absent student %d\n", absent_student);
+
+    return 0;
+
+}
---
+++ C/4.variables 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    double cgpa;
+    cgpa = 3.49;
+    printf("CGPA %lf\n", cgpa);
+
+    return 0;
+
+
+}
