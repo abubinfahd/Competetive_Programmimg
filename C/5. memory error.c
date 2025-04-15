a255b70900fac3b53db6917722ea95c79c74b6be
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-04-15 22:26:38 +0600

    Add files via upload

---
+++ C/5. memory error.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num;
+    num = 600000000; // memory error
+    printf("The number is %d", num);
+
+    return 0;
+
+}
---
+++ C/6. long long.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    long long num;
+    num = 200000000000;
+    printf("Num is %lld\n", num);
+
+    return 0;
+
+}
