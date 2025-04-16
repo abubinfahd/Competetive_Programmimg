6f88ee2a633bc6a0fe7a819571c73637bcb57410
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-04-16 14:40:16 +0600

    Add files via upload

---
+++ C/7. precision erroe.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x = 6;
+    printf("%d\n", x);
+    double y = 3.49;
+    int z = y;
+    printf("%d", z);// print 3 instead of 3.49
+
+    return 0;
+}
---
+++ C/8. type_casting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    double p = 5/2;
+    printf("%lf\n", p);
+
+    double q = 5.0 / 2;
+    printf("%lf\n", q);
+
+    return 0;
+}
+
