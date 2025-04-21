9c30811e78b31cb84bf86d0c7a002a8e50779ec0
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-04-21 11:43:20 +0600

    Add files via upload

---
+++ C/15. Increment-decrement 2.c
@@ -0,0 +1,19 @@
+/*
+#include<stdio.h>
+int main(){
+    int a = 7;
+    int b = a++;
+    printf("%d\n%d\n", a, b);
+
+    return 0;
+}
+*/
+#include<stdio.h>
+int main(){
+    int a = 7;
+    int b = ++a;
+    printf("%d\n%d\n", a, b);
+
+
+    return 0;
+}
