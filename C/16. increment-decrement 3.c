65c1f356076817a5cba6170d4930c2072f2bb946
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-04-26 20:34:52 +0600

    Add files via upload

---
+++ C/16. increment-decrement 3.c
@@ -0,0 +1,21 @@
+//#include<stdio.h>
+//int main(){
+//    int a;
+//    scanf("%d", &a);
+//    int ans = 2*(--a)+1; // a--
+//    printf("%d\n", ans);
+//
+//    return 0;
+//}
+#include<stdio.h>
+int main(){
+    int a = 7;
+    a+=3;
+    printf("%d\n", a);
+
+
+    return 0;
+}
+
+
+
