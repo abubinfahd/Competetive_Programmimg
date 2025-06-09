8e3a06e47d7c165d6bdc7686574dfe151a899d2f
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-06-09 09:49:53 +0600

    Bone Appetit

---
+++ Module 1 Contest/M_1_M.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n, m;
+    int x, y;
+    scanf("%d%d%d%d", &x, &y, &n, &m);
+    int result = (x*n) + (y*m);
+    printf("%d\n", result);
+
+
+    return 0;
+
+}
