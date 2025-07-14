742d64db16d10a6256cbdd488799af30a050a05c
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2025-07-14 14:31:02 +0600

    individual entry

---
+++ C/array2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int marks[3];
+    marks[0] = 39;
+    marks[1] = 32;
+    marks[2] = 34;
+    //printf("%d\n", marks[0]);
+    for(int i=0;i<=2;i++){
+        printf("%d\n", marks[i]);
+    }
+    return 0;
+}
+
+// start indexing from 0
